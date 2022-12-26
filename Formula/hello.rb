343a38e2f1ff0b6814444c99b7eb30a6ae28505b
Riyaz <riyazpasha880@gmail.com>
2022-12-26 15:24:10 +0530

    Update version

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -1,9 +1,9 @@
-class Hello < Formula
+class Pizza < Formula
     desc "A simple pizza app"
     homepage "https://github.com/riyaz-pasha/homebrew-pizza"
-    url "https://github.com/riyaz-pasha/homebrew-pizza/archive/refs/tags/1.0.0.tar.gz"
+    url "https://github.com/riyaz-pasha/homebrew-pizza/releases/download/3.0.0/pizza-macos-arm64.tar.gz"
     sha256 "94a17df00d498267d0a6c20d65eb47040ecb44f5af5cb641b7a12f1dde591ad7"
-    version "1.0.0"
+    version "3.0.0"
     def install
       bin.install "pizza"
     end
